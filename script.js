53c9e075cef2bdafd76d0e789098d561d0e7369e
unknown <gyebnarpetter@gmail.com>
2020-08-24 16:14:31 +0200

    header part

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".burger-icon").click(() => {
+    $("nav").slideToggle();
+    $(".nav-close-btn").slideToggle();
+});
+
+$(".nav-close-btn").click(() => {
+    $("nav").slideToggle();
+    $(".nav-close-btn").slideToggle();
+});
