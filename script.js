f53f1f3e0880f7f28dc987369864ba273cdfc8a7
unknown <gyebnarpetter@gmail.com>
2020-08-25 17:26:07 +0200

    slider

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// NAVIGATION
 $(".burger-icon").click(() => {
     $("nav").slideToggle();
     $(".nav-close-btn").slideToggle();
@@ -7,3 +8,31 @@ $(".nav-close-btn").click(() => {
     $("nav").slideToggle();
     $(".nav-close-btn").slideToggle();
 });
+
+$("nav a").click(() => {
+    $("nav").slideToggle();
+    $(".nav-close-btn").slideToggle();
+});
+
+
+
+// SLIDER
+let imagesIndex = 0;
+let images = document.getElementsByClassName("slide-page");
+
+let slider = () => {
+   for (let i = 0; i < images.length; ++i) {
+       images[i].style.display = "none";
+   }
+
+   if (imagesIndex === images.length) {
+       imagesIndex = 0;
+   }
+
+   images[imagesIndex].style.display = "block";
+   imagesIndex++;
+
+   setTimeout(slider, 10000);
+}
+
+slider();
